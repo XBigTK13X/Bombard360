b73cf08341f7e72a7f40be4defd56772ee0b9b97
XBigTK13X <xbigtk13x@gmail.com>
2011-03-27 00:33:05 -0400

    Refactored the drawing details out of XnaDrawable into a new AnimatedSprite class.

---
+++ Bombard360/Bombard360/Bombard360/AnimatedTexture.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework;
+
+namespace Bombard360
+{
+    class AnimatedTexture
+    {
+        private readonly string m_assetPath = @"GameplaySheet";
+
+        protected SpriteBatch m_renderTarget;
+        protected ContentManager m_contentManager;
+
+        private int m_currentFrame;
+        private SpriteInfo m_spriteInfo;
+        private Rectangle m_currentCell;
+        private Texture2D m_graphic;
+
+        public void LoadContent(ContentManager assetHandler,string assetName)
+        {
+            m_contentManager = assetHandler;
+            m_graphic = assetHandler.Load<Texture2D>(m_assetPath);
+            m_spriteInfo = SpriteSheetManager.GetSpriteInfo(assetName);
+            m_currentFrame = 0;
+        }
+
+        public void Draw(SpriteBatch target,Vector2 position)
+        {
+            m_renderTarget = target;
+            target.Begin();
+            m_currentCell = new Rectangle(m_currentFrame * m_spriteInfo.X, m_spriteInfo.SpriteIndex * m_spriteInfo.Y, m_spriteInfo.X, m_spriteInfo.Y);
+            Vector2 tempPosition = new Vector2(position.Y * SpriteInfo.Width, position.X * SpriteInfo.Height);
+            target.Draw(m_graphic, tempPosition, m_currentCell, Color.White);
+            target.End();
+        }
+
+        public void Animate()
+        {
+            if (m_spriteInfo.MaxFrame != 1)
+            {
+                m_currentFrame = (m_currentFrame + 1) % m_spriteInfo.MaxFrame;
+            }
+        }
+    }
+}
--- Bombard360/Bombard360/Bombard360/XnaDrawable.cs
+++ Bombard360/Bombard360/Bombard360/XnaDrawable.cs
@@ -10,38 +10,23 @@ namespace Bombard360
 {
     class XnaDrawable
     {
-
-        private readonly string m_assetPath = @"GameplaySheet";
-        private int m_currentFrame;
-        private SpriteInfo m_spriteInfo;
-        private Rectangle m_currentCell;
-        private Texture2D m_graphic;
+        private AnimatedTexture m_graphic = new AnimatedTexture(); 
 
         protected static readonly int COOLDOWN_TIME = 2;
         protected bool m_isActive = true;
         protected bool m_isBlocking = false;
         protected string m_assetName;
         protected Vector2 m_position = Vector2.Zero;
-        protected SpriteBatch m_renderTarget;
-        protected ContentManager m_contentManager;
 
         //Load the texture for the sprite using the Content Pipeline
         public void LoadContent(ContentManager assetHandler)
         {
-            m_contentManager = assetHandler;
-            m_graphic = assetHandler.Load<Texture2D>(m_assetPath);
-            m_spriteInfo = SpriteSheetManager.GetSpriteInfo(m_assetName);
-            m_currentFrame = 0;
+            m_graphic.LoadContent(assetHandler,m_assetName);
         }
         //Draw the sprite to the screen
         public void Draw(SpriteBatch target)
         {
-            m_renderTarget = target;
-            target.Begin();
-            m_currentCell = new Rectangle(m_currentFrame * m_spriteInfo.X, m_spriteInfo.SpriteIndex * m_spriteInfo.Y, m_spriteInfo.X, m_spriteInfo.Y);
-            Vector2 tempPosition = new Vector2(m_position.Y * SpriteInfo.Width, m_position.X * SpriteInfo.Height);
-            target.Draw(m_graphic, tempPosition, m_currentCell, Color.White);
-            target.End();
+            m_graphic.Draw(target,m_position);
         }
 
         protected void Initialize(int gridColumn, int gridRow, string type)
@@ -51,10 +36,7 @@ namespace Bombard360
         }
         public virtual void Update()
         {
-            if (m_spriteInfo.MaxFrame != 1)
-            {
-                m_currentFrame = (m_currentFrame + 1) % m_spriteInfo.MaxFrame;
-            }
+            m_graphic.Animate();
         }
         public void Move(int amountX, int amountY)
         {
